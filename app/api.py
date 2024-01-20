771800d7476a7cbbfc0d9becbf27467a0f941ad9
Priyanshu-hawk <priyanshupatel.hawk@gmail.com>
2024-01-20 17:57:25 +0530

    port update

--- app/api.py
+++ app/api.py
@@ -159,4 +159,4 @@ api.add_resource(PDFTranslateStatus, '/status')
 api.add_resource(AIToPDF, '/ai_to_pdf')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8008)
+    app.run(debug=True, host='0.0.0.0', port=8000)
